c82928354d22e93cadcc510b2be8b46f4d5446e7
amcjcy <82363754+amcjcy@users.noreply.github.com>
2021-04-21 22:27:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,20 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+mkdir -p files/root
+pushd files/root
+
+## Install oh-my-zsh
+# Clone oh-my-zsh repository
+git clone https://github.com/robbyrussell/oh-my-zsh ./.oh-my-zsh
+
+# Install extra plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ./.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ./.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions ./.oh-my-zsh/custom/plugins/zsh-completions
+
+# Get .zshrc dotfile
+cp ../../../data/zsh/.zshrc .
+
+popd
